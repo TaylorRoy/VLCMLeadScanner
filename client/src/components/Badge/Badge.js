4f54d3bc8d63c082963e98781a28a031c322daeb
TaylorRoy <tbroy@hotmail.com>
2019-01-21 20:13:39 -0700

    working on pdf generator

--- client/src/components/Badge/Badge.js
+++ client/src/components/Badge/Badge.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./badge.css";
 
 const Badge = props => (
-<div class="badgeContainer col-md-6"> 
-    <p class="badgeHeader">IT EXCHANGE</p>
-    <h1> {props.firstname}  {props.lastname}</h1>
-    <h2> {props.company}</h2>
-    <h1>{props.qrValue}</h1>
-</div>
-); 
+    <div class="badgeContainer col-md-6">
+        <p class="badgeHeader">IT EXCHANGE</p>
+            <h1> {props.firstname}  {props.lastname}</h1>
+            <h2> {props.company}</h2>
+        <h1>{props.qrValue}</h1>
+    </div>
+);
 
 export default Badge;
