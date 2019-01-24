bd40573e28e3cde3c68b434fd37ec60cd238b84f
jessicadoucette <jessicadoucette15@gmail.com>
2019-01-23 19:59:16 -0700

    merge shit

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component} from "react";
+import { Link } from 'react-router-dom'
 import API from "../utils/API";
 import Navbar from "../components/Navbar";
 import Consumer from './../GlobalState'
