9706aa5cef0e12e77b87350a66a5c4e32168ee84
TaylorRoy <tbroy@hotmail.com>
2019-01-23 20:16:45 -0700

    fixed merge issue

--- client/src/components/Badge/Badge.js
+++ client/src/components/Badge/Badge.js
@@ -3,7 +3,7 @@ import "./badge.css";
 
 const Badge = props => (
     <div className="badgeContainer col-md-6">
-        <p className="badgeHeader">IT EXCHANGE</p>
+        <img style={{width:"100%", height: "auto"}} src="https://files.slack.com/files-pri/TBLFR8X19-FFKN28WC9/screen_shot_2019-01-22_at_2.31.13_pm.png"/>
             <h1> {props.firstname}  {props.lastname}</h1>
             <h2> {props.company}</h2>
         <h1>{props.qrValue}</h1>
