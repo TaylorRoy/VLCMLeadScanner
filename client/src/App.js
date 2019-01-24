192e8b2dad9df00bf1b22eddf66df08c918fb250
russdc88 <russdc88@gmail.com>
2019-01-23 19:01:56 -0700

    correct fixes

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Badge from "./pages/Badge";
 import Admin from "./pages/Admin";
 import Sameday from "./pages/Sameday";
 import ManualLead from "./pages/ManualLead";
-import UploadLeadList from "./pages/UploadLeadList";
+
 import Navbar from "./components/Navbar";
 import Consumer, { GlobalState } from "./GlobalState";
 
@@ -28,7 +28,6 @@ const App = (props) => (
 							{(global.state.authenticated && global.state.adminAuthenticated) && <Route exact path="/admin" component={Admin} />}
 							{(global.state.authenticated && global.state.adminAuthenticated) && <Route exact path="/badge" component={Badge} />}
 							{(global.state.authenticated && global.state.adminAuthenticated) && <Route exact path="/sameday" component={Sameday} />}
-							{(global.state.authenticated && global.state.adminAuthenticated) && <Route exact path="/UploadLeadList" component={UploadLeadList} />}
 							{(global.state.authenticated) && <Route exact path="/ManualLead" component={ManualLead} />}
 
 
